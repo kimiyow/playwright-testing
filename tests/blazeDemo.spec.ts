4d3845c6e5589ff89652bd863108aaf559ecf00a
kimiyow <kimimontes04@gmail.com>
2026-04-27 14:56:54 -0700

    test

---
+++ tests/blazeDemo.spec.ts
@@ -0,0 +1,30 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+  await page.goto('https://blazedemo.com/index.php');
+  await page.locator('select[name="fromPort"]').selectOption('Mexico City');
+  await page.getByRole('button', { name: 'Find Flights' }).click();
+  await page.getByRole('row', { name: 'Choose This Flight 43 Virgin' }).getByRole('button').click();
+  await page.getByRole('textbox', { name: 'Name', exact: true }).click();
+  await page.getByRole('textbox', { name: 'Name', exact: true }).fill('Kim');
+  await page.locator('form div').filter({ hasText: 'Address' }).click();
+  await page.getByRole('textbox', { name: 'Address' }).dblclick();
+  await page.getByRole('textbox', { name: 'Address' }).fill('123');
+  await page.getByRole('textbox', { name: 'City' }).click();
+  await page.getByRole('textbox', { name: 'City' }).fill('Ens');
+  await page.getByRole('textbox', { name: 'State' }).click();
+  await page.getByRole('textbox', { name: 'State' }).fill('BC12');
+  await page.getByRole('textbox', { name: 'Zip Code' }).click();
+  await page.getByRole('textbox', { name: 'Zip Code' }).fill('323');
+  await page.getByRole('textbox', { name: 'State' }).click();
+  await page.getByRole('textbox', { name: 'Credit Card Number' }).click();
+  await page.getByRole('textbox', { name: 'Credit Card Number' }).fill('2133123123123');
+  await page.getByRole('textbox', { name: 'Month' }).click();
+  await page.getByRole('textbox', { name: 'Year' }).click();
+  await page.getByRole('textbox', { name: 'Name on Card' }).click();
+  await page.getByRole('textbox', { name: 'Name on Card' }).fill('2132');
+  await page.getByText('Name on Card').click();
+  await page.getByRole('textbox', { name: 'Name on Card' }).click();
+  await page.getByRole('textbox', { name: 'Name on Card' }).fill('Kim Kim');
+  await page.getByRole('button', { name: 'Purchase Flight' }).click();
+});
